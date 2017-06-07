df6d629e62954811d35df73713ebc79c4914537d
20161104600 <1428373962@qq.com>
2017-06-07 08:59:59 +0800

    Initial Commit

---
+++ tast0607c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tast0607c
+//
+//  Created by 20161104600 on 17/6/7.
+//  Copyright © 2017年 20161104600. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
